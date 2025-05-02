5999c08e4192c09bba5ef6457cb3367464dd7518
kaique <kaiquesouzapereira63@gmail.com>
2025-05-02 19:50:12 -0300

    Finish details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,8 @@ const Home = () => {
   return (
     <>
       <Head>
-<<<<<<< HEAD
         <title>Sobre Nós | Doc</title>
         <meta name="description" content="Somos um site que ajudamos a você a gerenciar seu dia e sua vida com o melhor de nós" />
-=======
-        <title>Sobre mim | Kaique</title>
-        <meta name="description" content="Sou um desenvolvedor Front-End apaixonado por criar interfaces e ajudar outros devs" />
->>>>>>> 46c41461b1218c19ee9854a2147a5cce4af68f4c
       </Head>
         <ProductivityLoginPage/>
     </>
